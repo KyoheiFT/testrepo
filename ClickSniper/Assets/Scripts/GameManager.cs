5333b804e419bfa83f4485c39b01030789878596
KyoheiFT <damdam0419@gmail.com>
2021-07-10 17:18:13 +0900

    Some Target
    複数のターゲットが表示される機能を追加

--- ClickSniper/Assets/Scripts/GameManager.cs
+++ ClickSniper/Assets/Scripts/GameManager.cs
@@ -4,12 +4,14 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    public GameObject target;//プレハブを入れるための変数
+    public GameObject[] targetArray;//プレハブを入れるための変数配列
+    private int count;//出現数のカウント
     private float time;//出現の間隔時間
+    private int vecY;//_Y座標を入れる変数
     // Start is called before the first frame update
     void Start()
     {
-        
+        count = 0;//countの初期値
     }
     // Update is called once per frame
     void Update()
@@ -18,9 +20,19 @@ public class GameManager : MonoBehaviour
         //timeが0秒になったら
         if(time <= 0.0f)
         {
-            //プレハブを出現させる
-            Instantiate(target,new Vector3(-40, 5, 30), Quaternion.identity);
+            vecY = Random.Range(0, 20);//0～12で値をランダム生成
+            //登録したターゲットを順番に生成。Y座標をvecYとする
+            Instantiate(targetArray[count],new Vector3(-40, vecY, 30), Quaternion.identity);
             time = 5.0f;//5秒に戻す
+            if(count <= 1)
+            {
+                count++;//カウントを１増やす
+            }
+            else if(count == 2)
+            {
+                count = 0;//カウントを０に戻す
+            }
+            
         }
     }
 }
